46fb829a120c47e72baf2fced7f6b0aeb86c4a37
JiaChau <jiacgsmst@gmail.com>
2025-04-17 02:28:09 -0400

    Change Wave Rates For Floors
    tetris broken

---
+++ Assets/Scripts/Tetris/BlockPiece.cs
@@ -0,0 +1,35 @@
+// Scripts/Tetris/BlockPiece.cs
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BlockPiece : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector2 startPosition;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = rectTransform.anchoredPosition;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        // Optional: Add snapping logic here (check valid tile under mouse)
+    }
+}
